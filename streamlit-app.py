77703b8679f07bcea6d6052aa8dcb61efcc27d46
NY1105 <yanwaiyin1105@gmail.com>
2023-02-28 12:39:48 +0800

    streamlit

---
+++ streamlit-app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import tensorflow as tf
+from keras_models.Preprocess import Preprocessing
+
+# @st.cache_resource
+def load_model_local(model_path):
+    model = tf.keras.models.load_model(model_path)
+    return model
+
+def preload_model(attributes=['cAGR', 'cCON', 'cEXT', 'cOPN', 'cNEU'], model_no=1):
+    preloaded = []
+    for attrbute in attributes:
+        model_path = f'./checkpoint/{attrbute}_model_{model_no}.tf'
+        preloaded.append(load_model_local(model_path))
+    return preloaded
+
+def preprocess(text):
+    prep = Preprocessing()
+    prep.load_text(text)
+    return prep
+# A, C, E, O, N = preload_model(model_no=2)
+model = load_model_local('./checkpoint/cAGR_model_2.tf')
+textObj = None
+
+st.title("Text-based Five-Factor Model Personality Prediction with BiLSTM")
+st.write("Created by Nicholas Yan for the 2023 Swire Hotel Hackathon")
+text = st.text_area(' ', placeholder='Enter text here to analyze ...')
+if st.button('Predict') or text:
+    if not textObj:
+        st.write(text)
+        textObj = preprocess(text)
+    st.write('Predicted personality traits:')
+    st.write('A',model.predict(textObj.X,verbose=2))
+    st.write('C',model.predict(textObj.X,verbose=2))
+    st.write('E',model.predict(textObj.X,verbose=2))
+    st.write('O',model.predict(textObj.X,verbose=2))
+    st.write('N',model.predict(textObj.X,verbose=2))
+
+
+
+
